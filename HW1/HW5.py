41fc92dca21f131215e6ae185024afba792ed729
Tony <SjasFaceMD@gmail.com>
2020-05-29 21:36:49 +0300

    Add files via upload

--- HW1/HW5.py
+++ HW1/HW5.py
@@ -74,24 +74,23 @@ from json import dump
 #file = input("Enter filename")
 file = "firms"
 debug = []
-with open(f"{file}.txt", 'r') as read_file:
+with open(f"files/{file}.txt", 'r') as read_file:
     firms, avg = {}, {}
     avg["average_profit"] = 0
-    c =1 
+    c =0 
     for line in read_file.readlines():
         data = line.split()
         if int(data[2]) - int(data[3].split(".")[0]) >0:
             firms[data[0]] = int(data[2]) - int(data[3].split(".")[0])
+            print(int(data[2]) - int(data[3].split(".")[0]))
             avg["average_profit"] += int(data[2]) - int(data[3].split(".")[0])
             c +=1
         else:pass
-    if avg["average_profit"] >0:
-        avg["average_profit"] = avg["average_profit"]/c
-        debug.append(avg)
-    else:
-        saved = avg["average_profit"]
-        avg["average_profit"] = f"Conglomerate loss: {saved}"
-        debug.append(avg)
+    print(c)
+    print(avg["average_profit"])
+    avg["average_profit"] = avg["average_profit"]/c
+    print(avg["average_profit"])
+    debug.append(avg)
     debug.insert(0,firms)
 print(debug)
 with open(f"out.json", 'w') as write_file:
